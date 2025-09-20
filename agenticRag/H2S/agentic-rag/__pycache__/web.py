e2b6c06078f9448af10fb90842ed89e43f27377b
SatyamAnilkumarMishra <satyamsam2612@gmail.com>
2025-09-20 23:34:52 +0530

    Delete agenticRag/H2S directory

--- agenticRag/H2S/agentic-rag/__pycache__/web.py
+++
@@ -1,7 +0,0 @@
-from duckduckgo_search import DDGS
-
-with DDGS() as ddgs:
-    results = ddgs.text("which is best collage in india give e name?", max_results=2)
-    for r in results:
-        print(r["title"], ":", r["href"])
-
